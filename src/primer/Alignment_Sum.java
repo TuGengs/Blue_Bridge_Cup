adc0bb6005e49913ba60ab2d2e8119735fdd192a
TuGengs <3189328105@qq.com>
2018-01-31 11:46:11 +0800

    序列求和

---
+++ src/primer/Alignment_Sum.java
@@ -0,0 +1,36 @@
+package primer;
+
+import java.util.Scanner;
+
+public class Alignment_Sum {
+
+	public static String doubleTrans(double d) {
+		
+		if (Math.round(d) - d == 0) {
+			
+			return String.valueOf((long) d);
+		}
+		
+		return String.valueOf(d);
+	}
+
+	public static void main(String[] args) {
+
+		Double a;
+		Double b;
+
+		a = new Scanner(System.in).nextDouble();
+		b = (Integer.valueOf(1) + a) * a * 0.5;
+
+		/**
+		 * 5.000000005E17 5
+		 * 
+		 * String str = String.valueOf(b); int v = str.indexOf('.');
+		 * 
+		 */
+		
+		System.out.println(doubleTrans(b));
+
+
+	}
+}
