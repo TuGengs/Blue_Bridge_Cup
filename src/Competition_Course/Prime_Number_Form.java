98720a44831436b97c3938875432c021ca185fa9
TuGengs <3189328105@qq.com>
2018-03-18 10:42:07 +0800

    素数筛选：用n ／ 2 表示 n 是否为素数。比较难想到，因为素数都是奇数，所以转换回去的时候都是n << 2 + 1。

--- src/Competition_Course/Prime_Number_Form.java
+++ src/Competition_Course/Prime_Number_Form.java
@@ -1,10 +1,56 @@
 package Competition_Course;
 
+import java.util.Scanner;
+
+// 1000 以内素数筛选法
 public class Prime_Number_Form {
 
+	public static boolean[] nList = new boolean[1000]; 
+	
 	public static void main(String[] args) {
 		
-//		System.out.println(String.valueOf(0.123));
+		Integer times = new Scanner(System.in).nextInt();
+		
+		if (times == 1) {
+			
+			System.out.println(2);
+			
+			return ;
+			
+		}
+		
+		int n = 0;
+		
+		int t,sq = (int)Math.sqrt(500 * 2 + 1);  
+		
+		for (n = 3; n <= sq; n += 2) {
+			
+	        if (nList[n>>1]) continue;
+	        
+	        for (t = n * n; t <= 500 << 1; t += n << 1) {//筛选循环 
+	            nList[t >> 1] = true;
+	        }
+	        
+	    }
+		
+		int tm = 1;
+		
+		for (t = 1; t < 500; ++t) {
+			
+	        if (nList[t]) continue;
+	        
+	        tm ++; 
+	        
+	        if (tm == times) {
+	        		
+	        		System.out.println((t << 1) + 1);
+	        
+	        		return ;
+	        		
+	        }
+	        
+	        
+	    }  
 		
 	}
 	
