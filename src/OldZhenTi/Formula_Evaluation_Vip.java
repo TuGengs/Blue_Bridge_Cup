294335ffae405c9574636e8cbadf2b6c4d187498
TuGengs <3189328105@qq.com>
2018-03-18 12:56:34 +0800

    公式求值及测试数据

--- src/OldZhenTi/Formula_Evaluation_Vip.java
+++ src/OldZhenTi/Formula_Evaluation_Vip.java
@@ -1,6 +1,6 @@
 package OldZhenTi;
 
-import java.text.DecimalFormat;
+import java.math.BigInteger;
 import java.util.Scanner;
 
 /**
@@ -11,44 +11,109 @@ import java.util.Scanner;
  *
  */
 public class Formula_Evaluation_Vip {
-	
-	//组合计数
-	static int fun(int n, int m) { 
+
+	public static BigInteger lucas(BigInteger n, BigInteger m, BigInteger p){
+		if(m.equals(BigInteger.ZERO)) 
+			return BigInteger.ONE;
+		
+		return BigInteger.valueOf(
+				f(n.mod(p).longValue(),m.mod(p).longValue()))
+				.multiply(lucas(n.divide(p),m.divide(p),p))
+				.mod(p);
+		
+	}
+
+	public static long f(long n,long m){
+		
+		if(m>n) return 1;
+		
+		if(n==m|| m==0) return 1;
+		
+		if(m>n-m) m=n-m;
+		
+		long tmpi=1,tmpn=1,s1=1,s2=1,ans=1;
+		
+		for (int i = 1; i<=m; i++) {
+			tmpi=i;
+			tmpn=n-i+1;
+			s1=s1*tmpi%999101;
+			s2=s2*tmpn%999101;
 			
-        if(m>n)return 0;    
-        
-        if(m==0)return 1;
-        
-        return fun(n-1,m-1) + fun(n-1,m);
-	        
+		}
+		
+		ans = s2*pow1(s1,999099)%999101;
+	
+		return ans%999101;
+	
+	}
+	public static long pow1(long x,long n) {
+		
+		if(x==1) return 1;
+		
+		if (n==0)
+			return 1;
+		
+		else {
+			while ((n & 1)==0) {
+				n>>=1;
+				x=(x *x)%999101;
+			}
+		
+		}
+		
+		long result = x%999101;
+		n>>=1;
+		while (n!=0) {
+			x=(x *x)%999101;;
+			if ((n & 1)!=0)
+				result =result*x%999101;
+			n>>=1;
+		}
+		return  result;
+	
 	}
 	
 	public static void main(String[] args) {
-		
 		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		
-		int m = sc.nextInt();
-		
-		int k = sc.nextInt();
-		
-		double sum = 0;
-		
-		int nm = fun(n, m);
-		
-		for (int i = 0; i <= n; i++) {
-			
-			sum = sum + fun(n, i) * nm * Math.pow(i, k);
-			
-			sum = sum % 999101;
-			
-		} 
-
-		DecimalFormat df = new DecimalFormat("0");
-		
-		System.out.println(df.format(sum));
+		BigInteger n = new BigInteger(sc.nextLine());
+		BigInteger m = new BigInteger(sc.nextLine());
+		int k = Integer.parseInt(sc.nextLine());
+//		long start = System.currentTimeMillis();
+		BigInteger md = new BigInteger("999101");
+		long Cnm=lucas(n, m,md).longValue()%999101;
+		long sum = 0;
+		if(Cnm!=0){
+			int[][] a = new int[k][k];
+			int h = 1;
+			for (int i = 0; i < k; i++) {
+				for (int j = 0; j < k; j++) {
+					if (j >= h)
+						a[i][j] =0;
+					else {
+						if (j == 0 || j == h - 1)
+							a[i][j] = 1;
+						else {
+							a[i][j] = (a[i - 1][j - 1]*(h - j)+a[i - 1][j])%999101;
+						}
+					}
+				}
+				h++;
+			}
+			long m1 = 1,n1 =1;
+			long x=n.subtract(new BigInteger(k+"")).mod(md.subtract(BigInteger.ONE)).longValue();
+			long n3 = pow1(2,x);
+			for (int i = k - 1; i >= 0; i--) {
+				n1=n3*pow1(2,i)%999101;
+				m1 = m1*(n.subtract(new BigInteger((k - 1 - i) + "")).mod(md).longValue())%999101;
+				sum = (sum+m1*a[k - 1][i]*n1)%999101;
+			}
+			sum = sum*Cnm%999101;
+		}
+		System.out.println(sum);
+//		long end = System.currentTimeMillis();
+//		System.out.println(end - start);
 		
 	}
 
 }
+
