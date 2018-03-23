b684ad3c4bc00886ab330cb207d74e3e230fb828
TuGengs <3189328105@qq.com>
2018-03-23 17:19:52 +0800

    不能买的数，数论知识，如果两个数不互质的话，就无穷大了

---
+++ src/OldZhenTi/The_Number_That_Can_Not_Be_Bought.java
@@ -0,0 +1,38 @@
+package OldZhenTi;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * T 买不到的数目
+ * 
+ * @author tugeng
+ *
+ */
+public class The_Number_That_Can_Not_Be_Bought {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		Integer a = sc.nextInt();
+		
+		Integer b = sc.nextInt();
+		
+		System.out.println(a * b - a - b);
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
